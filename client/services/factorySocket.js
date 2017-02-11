074e22bfce9e811df14009434d28048e08e821cf
Brenner Spear <blspear@gmail.com>
2017-02-10 16:10:19 -0800

    Stop stream correctly with stop button

--- client/services/factorySocket.js
+++ client/services/factorySocket.js
@@ -2,17 +2,23 @@ angular.module('myApp').factory('socket', function($http) {
 
   var factory = {};
 
+  factory.streaming = false;
   factory.localStream = null;
   factory.localAudioContext = null;
 
   factory.stopRecording = function() {
-    window.Stream.end();
-    this.localAudioContext.close();
-    this.localStream.getTracks()[0].stop();
+    if(this.streaming) {
+      window.Stream.end();
+      this.localAudioContext.close();
+      this.localStream.getTracks()[0].stop();
+      this.streaming = false;
+      console.log('recording stopped');
+    }
   }
 
   factory.runSocket = function() {
 
+    //comes out as F32, we need it in I16
     function convertFloat32ToInt16(buffer) {
       l = buffer.length;
       buf = new Int16Array(l);
@@ -22,57 +28,74 @@ angular.module('myApp').factory('socket', function($http) {
       return buf.buffer;
     }
 
-    var session = {
+    const permissions = {
       audio: true,
       video: false
     };
 
-    var recorderProcess = function(e) {
-      var left = e.inputBuffer.getChannelData(0);
-      window.Stream.write(convertFloat32ToInt16(left));
+    const recorderProcess = function(e) {
+      if(window.Stream.writable) {
+        console.log('streaming...');
+        //it's mono, so we only need left
+        var left = e.inputBuffer.getChannelData(0);
+        //stream the audio to our web socket
+        window.Stream.write(convertFloat32ToInt16(left));
+      }
     }
 
-    var initializeRecorder = function(stream) {
+    const initializeRecorder = function(stream) {
+      
+      console.log('initialize Recorder');
 
+      //set factory variable so we can stop it with the stop button
       factory.localStream = stream;
-      console.log('stream:', stream);
-      console.log('inside initializeRecorder');
 
-      var audioContext = window.AudioContext;
-      var context = new audioContext();
+      //create instance of audioContext
+      const audioContext = new window.AudioContext();
 
       //set to factory variable so we can close it with the stop button
-      factory.localAudioContext = context;
+      factory.localAudioContext = audioContext;
 
-      var audioInput = context.createMediaStreamSource(stream);
+      //where the audio stream is coming from. in this case, from
+      //the media stream from navigator
+      const audioInput = audioContext.createMediaStreamSource(stream);
 
-      var bufferSize = 2048;
-
-      // create a javascript node
-      var recorder = context.createScriptProcessor(bufferSize, 1, 1);
+      // create a recorder - think of it as a wiretap on the stream
+      const bufferSize = 2048;
+      const recorder = audioContext.createScriptProcessor(bufferSize, 1, 1);
 
       // specify the processing function
-      console.log('this: ', factory);
-      recorder.onaudioprocess = recorderProcess.bind(factory);
+      recorder.onaudioprocess = recorderProcess;
 
       // connect stream to our recorder
       audioInput.connect(recorder);
 
        // connect our recorder to the previous destination
-      recorder.connect(context.destination);
-      console.log('bottom of initializeRecorder');
+      recorder.connect(audioContext.destination);
     }
 
-    navigator.getUserMedia(session, initializeRecorder, err => {
-      console.log(err);
-    });
+    if(!this.streaming) {
+      //get stream from the global navigator object
+      //and send the stream to initalize our recorder
+      navigator.mediaDevices.getUserMedia(permissions)
+      .then(mediaStream => {
+        initializeRecorder(mediaStream);
+      })
+      .catch(err => {
+        console.error;
+      });
 
-    var client = new BinaryClient('ws://localhost:9001');
+      //open new socket
+      var client = new BinaryClient('ws://localhost:9001');
 
-    client.on('open', function() {
-      // for the sake of this example let's put the stream in the window
-      window.Stream = client.createStream();
-    });
+      client.on('open', function() {
+        console.log('opening socket on localhost:9001');
+        // for the sake of this example let's put the stream in the window
+        window.Stream = client.createStream();
+      });
+
+      this.streaming = true;
+    }
 
   };
 
--- server/binaryServer.js
+++ server/binaryServer.js
@@ -1,36 +1,52 @@
 var binaryServer = require('binaryjs').BinaryServer;
 var wav = require('wav');
+const Speech = require('@google-cloud/speech');
+require('dotenv');
+
+const request = {
+  config: {
+    encoding: 'LINEAR16',
+    sampleRate: 48000
+  }
+};
+
 
 bServer = binaryServer({port: 9001});
 
 bServer.on('connection', client => {
   console.log('new connection');
 
-  var filename = 'Recording: '.concat(Date(), '.wav');
-
-  var fileWriter = new wav.FileWriter(filename, {
-    channels: 1,
-    sampleRate: 48000,
-    bitDepth: 16
-  });
-
   client.on('stream', (stream, meta) => {
     console.log('new stream');
-    stream.pipe(fileWriter);
+
+    //Create instance of Google Speech
+    const speech = Speech();
+    //create a place to pipe audio data
+    const recognizeStream = speech.createRecognizeStream(request)
+    .on('error', console.error)
+    .on('data', (data) => {
+      //send text data to console
+      // console.log(data);
+      process.stdout.write(data.results);
+    });
+
+    //pipe audio from the websocket to google speech
+    stream.pipe(recognizeStream);
 
     //end on end or close
     stream.on('end', () => {
-      fileWriter.end();
-      console.log('ending stream');
+      //alert ending stream
+      console.log('\nending stream');
     });
 
     client.on('close', () => {
-      if(fileWriter != null) {
-        fileWriter.end();
-        console.log('closing client');
-      }
+      //never seem to need this but just in case
+      console.log('closing client');
     });
     
   });
 
 });
+
+
+// GOOGLE_APPLICATION_CREDENTIALS=/Users/BrennerSpear/hackreactor/speech-to-text-demo/PalFinder-f5b1b61fd250.json
